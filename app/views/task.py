6d3347f56d95e67fff3511eadfb894a7e95ab02e
Godooih <thalesgodoi1@gmail.com>
2025-10-29 20:03:30 -0300

    Backend NexusTech

--- app/views/task.py
+++ app/views/task.py
@@ -1,46 +1,53 @@
+# Em seu arquivo de views.py
+
 from rest_framework.viewsets import ModelViewSet
-from ..models import Task, TaskStatus, TaskStatusImage, CustomUser
-from ..serializers import ReadWriteSerializer, TaskReadSerializer, TaskWriteSerializer, TaskStatusSerializer, TaskStatusImageSerializer
+from ..models import Task, TaskStatus, CustomUser
+from ..serializers import ReadWriteSerializer, TaskReadSerializer, TaskWriteSerializer
 from rest_framework.permissions import IsAuthenticated
 from ..utils import isAdmin
 from ..filters import TaskFilters
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
 
+# (Suas outras views como TaskStatusView, etc. podem continuar como estão)
 
 class TaskView(ReadWriteSerializer, ModelViewSet):
-    #select * from Task;
-    queryset = Task.objects.all() #qual a tabela e a query
-    read_serializer_class = TaskReadSerializer #qual o serializer
-    write_serializer_class = TaskWriteSerializer #qual o serializer
+    """
+    ViewSet unificada para visualizar e editar Tarefas (Chamados).
+    """
+    queryset = Task.objects.all()
+    read_serializer_class = TaskReadSerializer
+    write_serializer_class = TaskWriteSerializer
     permission_classes = [IsAuthenticated]
-    filter_backends = [DjangoFilterBackend, OrderingFilter]    
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_class = TaskFilters
     ordering_fields = '__all__'
 
     def get_queryset(self):
-        items = self.request.GET.items()
-        for item in items:
-            print(f'parameters: {item}')
+        """
+        Filtra os chamados:
+        - Admin vê tudo.
+        - Usuário normal vê apenas os que ele criou.
+        """
         user = self.request.user
-        #select * from Task WHERE creator_FK = user.id
         if user.is_authenticated:
-            return Task.objects.all() if isAdmin(user.id) \
-                else Task.objects.filter(creator_FK=user.id)
+            if isAdmin(user.id):
+                return Task.objects.all().order_by('-creation_date')
+            return Task.objects.filter(creator_FK=user).order_by('-creation_date')
         return Task.objects.none()
     
-
     def perform_create(self, serializer):
-        user = self.request.user
-        custom_user = CustomUser.objects.get(id=user.id)
-        serializer.save(creator_FK=custom_user)
+        """
+        Salva um novo chamado e cria seu status inicial.
+        """
+        # 1. Salva o chamado, injetando o usuário logado como o criador.
+        #    O self.request.user JÁ É o objeto CustomUser, não precisa de outra consulta.
+        task = serializer.save(creator_FK=self.request.user)
 
-    
-
-class TaskStatusView(ModelViewSet):
-    queryset = TaskStatus.objects.all() #qual a tabela e a query
-    serializer_class = TaskStatusSerializer #qual o serializer
-
-class TaskStatusImageView(ModelViewSet):
-    queryset = TaskStatusImage.objects.all() #qual a tabela e a query
-    serializer_class = TaskStatusImageSerializer #qual o serializer
+        # 2. Cria o primeiro status para este chamado (Status: OPEN).
+        TaskStatus.objects.create(
+            task_FK=task, 
+            user_FK=self.request.user,
+            comment="Chamado criado com sucesso."
+            # O status padrão 'OPEN' será pego do seu model.
+        )
